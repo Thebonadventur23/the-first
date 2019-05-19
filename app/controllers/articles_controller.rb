4b7aaeaa8897878907210eddf76047963a6f5f3f
Thebonadventur23 <mailhumster@gmail.com>
2019-05-19 18:01:03 +0300

    the second commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+include ArticlesHelper
 
   def index
     @articles = Article.all
@@ -7,4 +8,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.destroy(params[:id])
+    redirect_to article_path
+  end
+
 end
